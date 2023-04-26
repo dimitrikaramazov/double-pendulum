5d900e6b3fdd541e64fb9bbb6803eeeb6ce8b40b
Hugobeliin <131696136+Hugobeliin@users.noreply.github.com>
2023-04-26 15:50:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -33,11 +33,11 @@ dt = 0.01
 #             y.append(y_i)
 #     return np.array(y)
 
-pen = DoublePendulum(3,4, N = 10000)
+pen = DoublePendulum(3,2, N = 10000, l = 0.5)
 
 #print(pen.sol[0,:])
 
-pen.sol = nm.rk4(pen.derivatives, pen.sol[0,:], dt, pen.N)
+pen.sol = nm.euler_forward(pen.derivatives, pen.sol[0,:], dt, pen.N)
 
 pen.compute_energy()
 
@@ -48,8 +48,13 @@ for i in range(pen.N):
     t.append(dt * i)
     E.append(e)
     
-plt.plot(t, pen.energy, t, E)
-plt.ylim()
-print(e)
+plt.plot(t, pen.energy, color = 'red', label = 'Calculated Value')
+plt.plot(t,E, color = 'blue', label = 'Real Value')
+plt.xlabel("Time ( s )")
+plt.ylabel("Energy ( J )")
+plt.title("Graph of the energy for Euler Forward method")
+plt.legend(loc = 'best')
+plt.grid()
+plt.ylim(-1,1)
 
 #plt.plot(pen.sol[:,0], pen.sol[:,2])
